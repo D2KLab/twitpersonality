5da1f76d981b99a94594b7d22430818d4ba6a0da
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2017-11-23 17:50:27 +0100

    convert to unix and add interpreter

--- PersonalityPrediction/extensiveTuning_OCEAN.sh
+++ PersonalityPrediction/extensiveTuning_OCEAN.sh
@@ -1,2 +1,4 @@
-python Tuning_LASSO_OCEAN.py fasttext dataset.vec yes
-python Tuning_SVM_OCEAN.py fasttext dataset.vec yes
+#!/bin/bash
+
+python Tuning_LASSO_OCEAN.py fasttext FastText/dataset.vec yes 
+python Tuning_SVM_OCEAN.py fasttext FastText/dataset.vec yes
